4445ebfbb16dcc65052d795550ff6aea4952982c
Derek Bruckner <derekbruckner@0587342026.wireless.umich.net>
2019-04-09 11:21:04 -0400

    added boolean

--- prog.py
+++ prog.py
@@ -6,6 +6,10 @@ def num_vowels(phrase):
     for character in phrase:
         if character in vowels:
             count = count + 1
-    return count
+            if count > 2:
+                truth_value =  True
+            else:
+                truth_value = False
+    return truth_value
 
-#print(num_vowels('aaron'))
+print(num_vowels('aaron'))
