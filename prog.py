6cff68b18d5852e820d3e95f4884c67ea5157000
Aaron <aarondmiller36@gmail.com>
2019-04-09 11:18:13 -0400

    updated with descriptions of code

--- prog.py
+++ prog.py
@@ -1,11 +1,12 @@
-import random
+import random #this imports random module
+import json #this imports json module
 
-def num_vowels(phrase):
-    count = 0
-    vowels = ['a','e','i','o','u']
+def num_vowels(phrase): #these lines create a new function num_vowels, which takes a string as an argument
+    count = 0 #this starts an accumulator for the function
+    vowels = ['a','e','i','o','u'] #this is a list of vowels for an accumulator
     for character in phrase:
         if character in vowels:
             count = count + 1
-    return count
+    return count #this returns the total count of vowels
 
 #print(num_vowels('aaron'))
